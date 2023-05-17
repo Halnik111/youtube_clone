dcbf2ae83ef0b19dd9c0823a4015e93bf230c2c9
Nikolas <halonikolas@gmail.com>
2023-05-17 16:10:50 +0200

    .

--- src/components/PlaylistPopup/PlaylistPopup.js
+++ src/components/PlaylistPopup/PlaylistPopup.js
@@ -71,8 +71,8 @@ const PlaylistPopup = ({setOpenPopup, video}) => {
     };
 
     const displayPlaylists = () => {
-        console.log(playlists.length);
-        if (playlists.length !== 0) {
+        console.log(playlists);
+        if (playlists && playlists.length !== 0) {
             return playlists.map(playlist => <PlaylistPopupCheckbox key={playlist._id} playlist={playlist} video={video}/>)
         }
         else {
