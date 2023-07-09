9816e82dccf58904973c4c4b1559f43dbafecf7c
Nikolas <halonikolas@gmail.com>
2023-07-09 17:35:51 +0200

    .

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASESecret,
+    apiKey: process.env.REACT_APP_FIREBASE,
     authDomain: "halnik-yt.firebaseapp.com",
     projectId: "halnik-yt",
     storageBucket: "halnik-yt.appspot.com",
